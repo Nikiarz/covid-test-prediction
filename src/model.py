410d60833925f1c4b8b85ce16e48bc82d959ffac
Nikiarz <arzaninikit@gmail.com>
2025-06-10 17:03:55 -0700

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 
+def train_model(data_path):
+    df = pd.read_csv(data_path)
+    df = pd.get_dummies(df, columns=["sex"], drop_first=True)
+
+    X = df.drop(columns=["test_result"])
+    y = df["test_result"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    model = RandomForestClassifier(n_estimators=100)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    print(classification_report(y_test, y_pred))
