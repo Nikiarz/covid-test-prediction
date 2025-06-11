c04186dc5bd761f2de6b27b98b7226a552af6f7a
Nikiarz <arzaninikit@gmail.com>
2025-06-10 17:03:31 -0700

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+def clean_data(input_path, output_path):
+    df = pd.read_csv(input_path)
+    df["test_result"] = df["test_result"].map({"negative": 0, "positive": 1})
+    df.to_csv(output_path, index=False)
+    print(f"Saved cleaned data to {output_path}")
 
